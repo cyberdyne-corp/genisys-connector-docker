c3413633028f54da27e971f01e826c82f57e9ab1
Anthony Lapenna <lapenna.anthony@gmail.com>
2015-04-19 18:36:27 +0200

    Added the /resources endpoint.

---
+++ genisysconnector.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from bottle import route, run
+from dockermanager import DockerManager
+
+
+@route('/resources', method='GET')
+def list():
+    containers = docker.list_containers()
+    print("Containers: %s" % containers)
+
+docker = DockerManager('unix://var/run/docker.sock')
+run(host='localhost', port=8080)
