f844d556a01f479a5f3fb2eecf038eaa5e0e8487
tony <alapenna@workit.fr>
2015-08-07 16:00:35 +0200

    Added environment and port binding reading from service definitions file.

--- services.py
+++ services.py
@@ -2,4 +2,12 @@ myService = {
     "name": "myService",
     "image": "backend:latest",
     "command": "java -jar /tmp/backend.jar",
+    "environment": {
+        "SERVICE_NAME": "myService",
+        "SERVICE_TAGS": "myTag",
+        "SERVICE_8081_IGNORE": 1,
+        "SERVICE_8080_CHECK_CMD": "/tmp/health-check.sh",
+        "SERVICE_8080_CHECK_INTERVAL": "15s"
+    },
+    "ports": ['8080']
 }
