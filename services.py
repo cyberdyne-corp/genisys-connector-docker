681c3d8020e75ba0a7e118e63d24830728ad6b60
Anthony Lapenna <lapenna.anthony@gmail.com>
2015-08-09 12:00:18 +0200

    Refactored service name from myService to skynet_backend.

--- services.py
+++ services.py
@@ -1,10 +1,9 @@
-myService = {
-    "name": "myService",
-    "image": "backend:latest",
+skynet_backend = {
+    "name": "skynet_backend",
+    "image": "skynet-backend:latest",
     "command": "java -jar /tmp/backend.jar",
     "environment": {
-        "SERVICE_NAME": "myService",
-        "SERVICE_TAGS": "myTag",
+        "SERVICE_NAME": "skynet_backend",
         "SERVICE_8081_IGNORE": 1,
         "SERVICE_8080_CHECK_CMD": "/tmp/health-check.sh",
         "SERVICE_8080_CHECK_INTERVAL": "15s"
